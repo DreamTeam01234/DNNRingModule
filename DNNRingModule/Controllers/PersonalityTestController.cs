9b985ba43649d65469be82f96eea5acdfb2b2b76
Előd <simplyrings2025@gmail.com>
2025-04-22 16:07:30 +0000

    Töröltem az Item-hez tartozó fileokat. Átírtam az sqlsdataprovidert, hogy megfelelő sql tábla legyen. Ennek megfelelően átírtam a dnn file-t.

---
+++ DNNRingModule/Controllers/PersonalityTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Ring.Module.DNNRingModule.Components;
+using Ring.Module.DNNRingModule.Models;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Framework.JavaScriptLibraries;
+
+namespace Ring.Module.DNNRingModule.Controllers
+{
+    public class PersonalityTestController : DnnController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DNNRingModule/Models/PersonalityTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetNuke.ComponentModel.DataAnnotations;
+
+namespace Ring.Module.DNNRingModule.Models
+{
+    [TableName("PersonalityTest")]
+    [PrimaryKey("Id", AutoIncrement = true)]
+    public class PersonalityTestAnswer
+    {
+        public int Id { get; set; }
+        public int FilledOutBy { get; set; }
+        public DateTime FilledOutAt { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string Answer5 { get; set; }
+        public string Answer6 { get; set; }
+        public string Answer7 { get; set; }
+        public string Answer8 { get; set; }
+        public string Answer9 { get; set; }
+        public string Answer10 { get; set; }
+    }
+}
