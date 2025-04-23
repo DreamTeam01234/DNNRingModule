684c25dceb3ac3ef6bc45e3c401dc8d575d5f4bc
Előd <simplyrings2025@gmail.com>
2025-04-23 15:08:54 +0000

    A View-kat megváltoztattam, így a teszt kitöltése után lesz csak új view. Felkerültek a kérdések, valamint a controllert is átírtam.

--- DNNRingModule/Controllers/PersonalityTestController.cs
+++ DNNRingModule/Controllers/PersonalityTestController.cs
@@ -12,17 +12,45 @@ namespace Ring.Module.DNNRingModule.Controllers
 {
     public class PersonalityTestController : DnnController
     {
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.User = UserController.Instance.GetCurrentUserInfo();
             ViewBag.IsLoggedIn = ViewBag.User != null && ViewBag.User.UserID > 0;
-
+            ViewBag.Step = TempData["Step"] != null ? Convert.ToInt32(TempData["Step"]) : 0;
             return View();
         }
-        public ActionResult TestStart()
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            int step = 0;
+            int.TryParse(form["step"], out step);
+
+            if (step == 0)
+            {
+                TempData["Step"] = 1;
+                return RedirectToAction("Index");
+            }
+
+            if (step == 1)
+            {
+                TempData["Step"] = 2;
+                return RedirectToAction("Index");
+            }
+
+            if (step == 2)
+            {
+                return RedirectToAction("ThankYou");
+            }
+
+            TempData["Step"] = 0;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ThankYou()
         {
             return View();
         }
     }
 }
-
